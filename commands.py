2059ad137ca11d0e9db0d2558edcd7b980d53226
Maria <andreeva-mm03@yandex.ru>
2020-05-04 12:40:06 +0300

    Комментарии и поправки в requirements

--- commands.py
+++ commands.py
@@ -37,24 +37,25 @@ def messages(update, context):
         update.message.reply_text("Хорошо, вернёмся.", reply_markup=markup_main_keyboard)
         constant = None
     elif constant == 'film_search':
-        movie_list = Movie.objects.search(mes)
+        movie_list = Movie.objects.search(mes)  # Список из всех фильмов результатов поиска, чаще всего имеет длину 11
         length = len(movie_list)  # Чтобы постоянно не пересчитывать длину функции
         if length == 0:
             update.message.reply_text('Извините, фильма с таким названием не найдено. Вернёмся к началу.',
                                       reply_markup=markup_main_keyboard)
             constant = None
         elif length >= 1:
-            movie = Movie(id=movie_list[0].id)
-            movie.get_content('main_page')
+            movie = Movie(id=movie_list[0].id)  # Чтобы вызвать команду в следующей строке, нужно найти фильм по id
+            movie.get_content('main_page')  # Обязательная команда, чтобы получить информацию кроме года и названия
             update.message.reply_text(
                 'Полное название - {}.\n{}\nСлоган - {}\nГод - {}\nДлительность - {}\nРейтинг фильма - {}'
                 '\nХотите увидеть список актёров?'.format(
                     movie.title, movie.plot, movie.tagline,
                     movie.year, movie.runtime, movie.rating), reply_markup=markup_yesornot)
-            constant = ['cast', movie]
+            constant = ['cast', movie.actors]
+            # Передаём список, чтобы суметь обратить к списку актёров, если он нам понадобится
     elif constant[0] == 'cast':
         res = ''
-        x = constant[1].actors
+        x = constant[1]
         for el in x[:10]:
             res += '\n{}'.format(el)
         res = 'В фильме снимались:' + res
@@ -73,38 +74,54 @@ def messages(update, context):
             update.message.reply_text(
                 ('{}, {} год рождения\nХотите увидеть фильмографию?'.format(actor.name, actor.year_birth)),
                 reply_markup=markup_yesornot)
-            constant = ['filmography', actor]
+            constant = ['filmography', actor.career['actor']]
     elif constant[0] == 'filmography':
         res = ''
-        x = constant[1].career['actor']
+        x = constant[1]
         # Переменные, чтобы не использовать функцию len() много раз
         length_x = len(x)
         length_con = len(constant)
+        # Так как у актёра может быть очень много фильмов и
+        # их построчный вывод может занять долгое время и несколько страниц,
+        # что окажется неудобным для пользователя, мы будем выводить по 10 фильмов за раз
         if length_con == 2:
+            # Если мы выводим первую страницу фильмографии, длина константы будет равна двум,
+            # так что мы будем выводить первые 10 (или все, если length_x <= 10)
             if length_x > 10:
-                for el in constant[1].career['actor'][:10]:
+                for el in x[:10]:
                     res += '\n{}'.format(el.movie)
             else:
-                for el in constant[1].career['actor']:
+                for el in x:
                     res += '\n{}'.format(el.movie)
         elif constant[2] + 10 < length_x:
-            for el in constant[1].career['actor'][constant[2]:constant[2] + 10]:
+            # Если невыведенных элементов в списке всё ещё больше 10, мы выводим только следующие 10
+            for el in x[constant[2]:constant[2] + 10]:
                 res += '\n{}'.format(el.movie)
         else:
-            for el in constant[1].career['actor'][constant[2]:length_x]:
+            # Если невыведенных элементов в списке осталось 10 или меньше, мы выводим последние элементы
+            for el in x[constant[2]:length_x]:
                 res += '\n{}'.format(el.movie)
         if length_con == 2:
+            # Длина константы равна двум, если мы выводим первую страницу фильмографии.
+            # В таком случае мы добавим в начало фразу "Вот фильмография:"
             res = 'Вот фильмография:' + res
             if length_x > 10:
                 update.message.reply_text(res, reply_markup=markup_pages)
                 constant = constant + [10]
+                # Если в списке фильмов больше 10 кинокартин, мы выводим клавиатуру с предложением продолжить вывод
+                # и добавляем третий элемент к константе - количество уже выведенных фильмов
             else:
-                update.message.reply_text(res, reply_markup=markup_main_keyboard)
+                update.message.reply_text(res + '\n\nФильмография кончилась.', reply_markup=markup_main_keyboard)
                 constant = None
+                # Если в списке меньше 10 кинокартин, мы выводим основную клавиатуру и меняем константу на None
         elif constant[2] + 10 < length_x:
             update.message.reply_text(res, reply_markup=markup_pages)
             constant = constant[0:2] + [constant[2] + 10]
+            # Если невыведенные элементы в списке ещё есть, мы изменяем последний элемент константы
+            # и показываем клавиатуру с предложением продолжить вывод
         else:
             res += '\n\nФильмография кончилась.'
             update.message.reply_text(res, reply_markup=markup_main_keyboard)
             constant = None
+            # Если невыведенных элементов в списке не осталось, мы выводим основную клавиатуру
+            # и меняем константу на None
